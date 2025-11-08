9cafef8e06821c3260700b90bbdbcf5bdaeec0f7
Amal <amal2004@gmail.com>
2025-11-07 19:37:30 -0500

    Firefox and Edge browsers added

--- src/test/java/nopcom/autm/driver/WebDriverFactory.java
+++ src/test/java/nopcom/autm/driver/WebDriverFactory.java
@@ -5,6 +5,10 @@ import nopcom.autm.config.ConfigReader;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -42,9 +46,26 @@ public class WebDriverFactory {
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ConfigReader.getImplicitWait()));
             driver.manage().window().maximize();
             tlDriver.set(driver);
-        } else {
+        } else if ("edge".equalsIgnoreCase(browser)) {
+        	
+        	 EdgeOptions edgeOptions = new EdgeOptions();
+             edgeOptions.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage",
+                                      "--disable-gpu", "--window-size=1920,1080");
+             WebDriver driver = new EdgeDriver(edgeOptions);
+             tlDriver.set(driver);
+        }else if ("firefox".equalsIgnoreCase(browser)) {
+        	  FirefoxOptions ffOptions = new FirefoxOptions();
+              if (Boolean.getBoolean("headless")) {
+                  ffOptions.addArguments("--headless");
+                  ffOptions.addArguments("--width=1920");
+                  ffOptions.addArguments("--height=1080");
+              }
+              WebDriver driver = new FirefoxDriver(ffOptions);
+              tlDriver.set(driver);
+        }else {
             throw new RuntimeException("Unsupported browser: " + browser);
         }
+
     }
 
     public static void quitDriver() {
